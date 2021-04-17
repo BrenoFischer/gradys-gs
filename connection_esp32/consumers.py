65b4c9dd6a7ea9a915da2dca8a485813817ab308
BrenoFischer <breno_perricone@hotmail.com>
2021-04-16 21:54:40 -0300

    lidar com desconexão serial

--- connection_esp32/consumers.py
+++ connection_esp32/consumers.py
@@ -10,7 +10,7 @@ class ReceiveCommandConsumer(AsyncWebsocketConsumer):
     await self.accept()
 
   async def receive(self, text_data):
-    await handleConnection.aio_instance.write_async(text_data.encode())
+    await serial.aio_instance.write_async(text_data.encode())
 
   async def disconnect(self, close_code):
     print(f'Receive command websocket disconnected {close_code}')
@@ -20,11 +20,23 @@ class ConnectionConsumer(AsyncWebsocketConsumer):
   async def connect(self):
     await self.accept()
 
-    if handleConnection.connect_to_esp() == True:
-      queue = asyncio.Queue()
-      reader = asyncio.create_task(handleConnection.read_json(queue))
-      consumer = asyncio.create_task(handleConnection.consume(queue, self))
-      await asyncio.gather(reader)
+    serial.is_connected = serial.connect_serial()
+    while True:
+      if serial.is_connected:
+        try:
+          reader = asyncio.create_task(serial.read())
+          consumer = asyncio.create_task(serial.consume(self))
+          serial.queue = asyncio.Queue()
+          serial.tasks.extend([reader, consumer])
+          await asyncio.gather(reader)
+          await serial.handle_disconnection_exception()
+          serial.is_connected = False
+        except Exception as e:
+          print(e)
+          await serial.handle_disconnection_exception()
+          await serial.keep_trying_connection()
+      else:
+        await serial.keep_trying_connection()
 
   async def disconnect(self, close_code):
     print(f'Connection websocket disconnected {close_code}')
@@ -33,38 +45,55 @@ class ConnectionConsumer(AsyncWebsocketConsumer):
 class SerialConnection():
   def __init__(self):
     self.aio_instance = None
+    self.is_connected = False
+    self.tasks = []
+    self.queue = None
 
-  async def read_json(self, queue):
+
+  async def handle_disconnection_exception(self):
+    await self.queue.join()
+    for task in self.tasks:
+        task.cancel()
+
+
+  async def keep_trying_connection(self):
+    self.is_connected = False
+    while not self.is_connected:
+        self.is_connected = self.connect_serial()
+        await asyncio.sleep(3)
+
+
+  async def read(self):
     while True:
       raw_data: bytes = await self.aio_instance.readline_async()
       decoded_line = raw_data.decode('ascii')
       try:
         json_line = json.loads(decoded_line)
-        await queue.put(json_line)
+        await self.queue.put(json_line)
       except ValueError as e:
         print(e)
         print(decoded_line)
 
 
-  async def consume(self, queue, obj):
+  async def consume(self, obj):
     while True:
-      json_consumed = await queue.get()
-      queue.task_done()
+      json_consumed = await self.queue.get()
+      self.queue.task_done()
       print(f'consumed {json_consumed}')
       await obj.send(json.dumps(json_consumed))
 
 
-  def connect_to_esp(self):
+  def connect_serial(self):
     print("Tentando conexão com a serial...")
     try:
       self.aio_instance = aioserial.AioSerial(port='COM4', baudrate=115200)
       self.aio_instance.flush()
       print("Conexão estabelecida")
       return True
-    except serial.serialutil.SerialException as e:
+    except Exception as e:
       print("Não foi possível conectar")
       print(e)
       return False
 
 
-handleConnection = SerialConnection()
+serial = SerialConnection()
