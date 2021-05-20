3b6e5d9f4ca83819b2d8c1bac62066c288a169b0
BrenoFischer <breno_perricone@hotmail.com>
2021-05-20 18:03:27 -0300

    new msg protocol and json types cases

--- static/connection_esp32/js/main.js
+++ static/connection_esp32/js/main.js
@@ -5,30 +5,35 @@ observableSocket.onmessage = function(msg) {
   try {
     var djangoData = JSON.parse(msg.data);
     console.log(djangoData);
-  
-    if(djangoData['type'] == 13) { //Conectando
-      document.querySelector('#connection-status').innerText = "Esperando conexão serial...";
-    }
-    else if(djangoData['type'] == 14) { //Conectado
-      document.querySelector('#connection-status').innerText = "Conectado";
-    }
-    else {
-      if(djangoData['type'] == 249) {
+    json_type = djangoData['type'];
+    switch(json_type) {
+      case 13:  //Esperando conexão
+        document.querySelector('#connection-status').innerText = "Esperando conexão serial...";
+        break;
+      case 14:  //Conectado
+        document.querySelector('#connection-status').innerText = "Conectado";
+        break;
+      case 21:  //cmd-led-on-ACK
         document.querySelector('#actions-logs').innerText = "Acendeu LED";
-      }
-      else if(djangoData['type'] == 250) {
+        break;
+      case 23:  //cmd-led-off-ACK
         document.querySelector('#actions-logs').innerText = "Apagou LED";
-      }
-      //id = djangoData['id'];
-      //type = djangoData['type'];
-      //seq = djangoData['seq'];
-      //ack = djangoData['ACK'];
-      //sdata = djangoData['SDATA'];
-      //lat = djangoData['lat'];
-      //lng = djangoData['lng'];
-      //high = djangoData['high'];
-  
-      //document.querySelector('#actions-logs').innerText = `{'id': '${id}', 'type': ${type}, 'seq': ${seq}, 'ack': ${ack}, 'sdata': ${sdata}, 'lat': ${lat}, 'lng': ${lng}, 'high': ${high}}`;
+        break;
+      case 25: //forward 1 ACK
+        document.querySelector('#actions-logs').innerText = "Forward 1 recebido";
+        break;
+      case 27: //forward 2 ACK
+        document.querySelector('#actions-logs').innerText = "Forward 2 recebido";
+        break;
+      case 29: //Voo iniciado ACK
+        document.querySelector('#actions-logs').innerText = "Voo iniciado";
+        break;
+      case 31: //Voo abortado ACK
+        document.querySelector('#actions-logs').innerText = "Voo abortado";
+        break;
+      default:
+        document.querySelector('#actions-logs').innerText = "JSON unknown: " + msg.data;
+        break;
     }
   } catch(e) {
     document.querySelector('#actions-logs').innerText = msg.data;
@@ -45,11 +50,10 @@ sendCommandSocket.onclose = function(e) {
 
 
 // Onclick functions
-
 document.querySelector('#turn-on').onclick = function(e) {
   if (sendCommandSocket.readyState == WebSocket.OPEN) {
     sendCommandSocket.send(JSON.stringify(
-      {'id': 1, 'type': 249, 'seq': 0, 'ACK': 0, 'SDATA': 0, 'lat': -9, 'lng': 10, 'high': 11}
+      {'id': 1, 'type': 20, 'seq': 0, 'lat': -9, 'log': 10, 'high': 11, 'DATA': "0"}
     ));
   }
 };
@@ -57,7 +61,7 @@ document.querySelector('#turn-on').onclick = function(e) {
 document.querySelector('#turn-off').onclick = function(e) {
   if (sendCommandSocket.readyState == WebSocket.OPEN) {
     sendCommandSocket.send(JSON.stringify(
-      {'id': 1, 'type': 250, 'seq': 0, 'ACK': 0, 'SDATA': 0, 'lat': -9, 'lng': 10, 'high': 11}
+      {'id': 1, 'type': 22, 'seq': 0, 'lat': -9, 'log': 10, 'high': 11, 'DATA': "0"}
     ));
   }
 };
@@ -65,7 +69,7 @@ document.querySelector('#turn-off').onclick = function(e) {
 document.querySelector('#forward-1').onclick = function(e) {
   if (sendCommandSocket.readyState == WebSocket.OPEN) {
     sendCommandSocket.send(JSON.stringify(
-      {'id': 1, 'type': 251, 'seq': 0, 'ACK': 0, 'SDATA': 0, 'lat': -9, 'lng': 10, 'high': 11}
+      {'id': 1, 'type': 24, 'seq': 0, 'lat': -9, 'log': 10, 'high': 11, 'DATA': "0"}
     ));
   }
 };
@@ -73,7 +77,7 @@ document.querySelector('#forward-1').onclick = function(e) {
 document.querySelector('#forward-2').onclick = function(e) {
   if (sendCommandSocket.readyState == WebSocket.OPEN) {
     sendCommandSocket.send(JSON.stringify(
-      {'id': 1, 'type': 252, 'seq': 0, 'ACK': 0, 'SDATA': 0, 'lat': -9, 'lng': 10, 'high': 11}
+      {'id': 1, 'type': 26, 'seq': 0, 'lat': -9, 'log': 10, 'high': 11, 'DATA': "0"}
     ));
   }
 };
@@ -81,7 +85,7 @@ document.querySelector('#forward-2').onclick = function(e) {
 document.querySelector('#initiate-flight').onclick = function(e) {
   if (sendCommandSocket.readyState == WebSocket.OPEN) {
     sendCommandSocket.send(JSON.stringify(
-      {'id': 1, 'type': 253, 'seq': 0, 'ACK': 0, 'SDATA': 0, 'lat': -9, 'lng': 10, 'high': 11}
+      {'id': 1, 'type': 28, 'seq': 0, 'lat': -9, 'log': 10, 'high': 11, 'DATA': "0"}
     ));
   }
 };
@@ -89,7 +93,7 @@ document.querySelector('#initiate-flight').onclick = function(e) {
 document.querySelector('#interrupt-flight').onclick = function(e) {
   if (sendCommandSocket.readyState == WebSocket.OPEN) {
     sendCommandSocket.send(JSON.stringify(
-      {'id': 1, 'type': 254, 'seq': 0, 'ACK': 0, 'SDATA': 0, 'lat': -9, 'lng': 10, 'high': 11}
+      {'id': 1, 'type': 30, 'seq': 0, 'lat': -9, 'log': 10, 'high': 11, 'DATA': "0"}
     ));
   }
 };
