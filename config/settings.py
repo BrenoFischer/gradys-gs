1e753cae984c99c3d3ae0a45a51c0d087641bfb5
BrenoFischer <breno_perricone@hotmail.com>
2021-06-01 21:42:53 -0300

    first layout with google maps

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
+GOOGLE_MAPS_API_KEY = env("GOOGLE_MAPS_API_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- connection_esp32/views.py
+++ connection_esp32/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def index(request):
   return render(request, 'index.html', context={})
 
 def connection(request):
-  return render(request, 'connection.html', context={})
+  context = {
+    'google_maps_key': settings.GOOGLE_MAPS_API_KEY
+  }
+  return render(request, 'connection.html', context=context)
