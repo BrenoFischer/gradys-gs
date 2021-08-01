3de56f0a6910376b003dd7129036cec6a8bc71d4
BrenoFischer <breno_perricone@hotmail.com>
2021-08-01 19:36:51 -0300

    add inactive/on_hold params on ini file

--- connection_esp32/consumers.py
+++ connection_esp32/consumers.py
@@ -128,12 +128,15 @@ class UpdatePeriodcallyConsumer(AsyncWebsocketConsumer):
     print(f'Update periodically websocket disconnected {close_code}')
 
   async def send_json_list(self):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
     date_format = "%Y-%m-%dT%H:%M:%S.%f"
 
     while True:
       time_now = datetime.now()
-      seconds_to_be_inactive = 50
-      seconds_to_be_on_hold = 25
+      seconds_to_be_inactive = int(config['consumers']['seconds_to_device_be_inactive'])
+      seconds_to_be_on_hold = int(config['consumers']['seconds_to_device_be_on_hold'])
 
       for json_update in json_list_persistent:
         time_shifted_inactive = datetime.strptime(json_update['time'], date_format) + timedelta(seconds = seconds_to_be_inactive)
